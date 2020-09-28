6f4d97f4055df8d10a4d1d9d11ac332c4d96ba7d
liber.roi <liber.roi@gmail.com>
2020-09-28 07:22:49 +0300

    Updates

---
+++ precache-manifest.6b70514dc73b522224d62ee4f91a8fbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0edf51a05aa312ff8bd42342693d2a7",
+    "url": "/Snny/index.html"
+  },
+  {
+    "revision": "c0c8aaf5ce1018201175",
+    "url": "/Snny/static/css/main.7b2ac3e8.chunk.css"
+  },
+  {
+    "revision": "a078264d0e571642dc37",
+    "url": "/Snny/static/js/2.40e9b86d.chunk.js"
+  },
+  {
+    "revision": "1cad250318cbbd3f8b3f9b81a91b3ff6",
+    "url": "/Snny/static/js/2.40e9b86d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0c8aaf5ce1018201175",
+    "url": "/Snny/static/js/main.6483cf75.chunk.js"
+  },
+  {
+    "revision": "f1cc5efc4253d0f15068",
+    "url": "/Snny/static/js/runtime-main.8969bc3a.js"
+  },
+  {
+    "revision": "9118e9d8f26b7d3f98899b5f94a65f59",
+    "url": "/Snny/static/media/icon.9118e9d8.png"
+  }
+]);
